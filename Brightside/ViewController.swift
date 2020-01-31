aa360a11a37ef45089df1edac840162e24bcedd6
Blake Branvold <blake@branvold.com>
2020-01-31 07:43:22 -0600

    Initial Commit

---
+++ Brightside/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brightside
+//
+//  Created by 90304593 on 1/31/20.
+//  Copyright © 2020 90304593. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
